c99f1ef5aedc53c98d4494311a6c4980ea91dd6c
Surge Raval <Surge1223@gmail.com>
2018-07-08 06:59:29 +0000

    taimen: add CarrierConfig overlay for vzw only

--- device-carbon.mk
+++
@@ -1,9 +0,0 @@
-# Camera
-PRODUCT_PACKAGES += libion
-
-# Fingerprint sensor type
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hardware.fingerprint=fpc
-
-# Update engine
-PRODUCT_PACKAGES += brillo_update_payload
